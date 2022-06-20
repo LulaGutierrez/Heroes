242987ca8e732ad14b2eb2122a5cc00e31eeeaa1
LulaGutierrez <lujanvicgutierrez@gmail.com>
2022-06-20 15:05:26 -0300

    agregue imagenes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require("path");
 const port = 3030;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "index.html")))
 app.get("/babbage", (req, res) => res.sendFile(path.join(__dirname, "views", "babbage.html")))
 app.get("/berners-lee", (req, res) => res.sendFile(path.join(__dirname, "views", "berners-lee.html")))
